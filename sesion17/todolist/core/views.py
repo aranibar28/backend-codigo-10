a9411d8bca6546aa856fd960aed0c43c5586d9db
Gerson Aranibar <aranibargerson28@gmail.com>
2021-11-03 19:38:41 -0500

    add models, views and migrations

--- sesion17/todolist/core/views.py
+++ sesion17/todolist/core/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.views import View
+from core.models import Task
 
 # Create your views here.
+class TaskView(View):
+    def get(self, request):
+        tasks = Task.objects.all()
+        context = {
+            "tasks": tasks,
+        }
+        return render(request, "index.html", context)
+
+    def post(self, request):
+        data = request.POST
+        task = Task(title=data['title'])
+        task.save()
+        return redirect('/tasks/')
