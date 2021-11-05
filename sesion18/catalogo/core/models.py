98a2bf50bd148e545f69a7eccee817705598eb06
Gerson Aranibar <aranibargerson28@gmail.com>
2021-11-04 19:35:28 -0500

    migrations

--- sesion18/catalogo/core/models.py
+++ sesion18/catalogo/core/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Product(models.Model):
+    name = models.CharField(max_length=100)
+    price = models.FloatField()
+    sku = models.CharField(max_length=11)
+    exp_date = models.DateField()
+    
