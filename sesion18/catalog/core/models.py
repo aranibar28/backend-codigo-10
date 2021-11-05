5a6ce0bebcb54649e3055bffd6e5a7d844004695
Gerson Aranibar <aranibargerson28@gmail.com>
2021-11-05 15:23:36 -0500

    update category models

--- sesion18/catalog/core/models.py
+++ sesion18/catalog/core/models.py
@@ -1,4 +1,10 @@
 from django.db import models
+from django.db.models.deletion import CASCADE
+
+# Create your models here. (GENERIC)  
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
 
 # Create your models here.
 class Product(models.Model):
@@ -6,8 +12,4 @@ class Product(models.Model):
     price = models.FloatField()
     sku = models.CharField(max_length=11)
     exp_date = models.DateField()
-
-# Create your models here. (GENERIC)  
-class Category(models.Model):
-    name = models.CharField(max_length=100)
-    description = models.TextField()
+    category = models.ForeignKey(Category, on_delete=CASCADE)
