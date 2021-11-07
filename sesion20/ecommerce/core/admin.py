a2b510ed26d4142855960fb5344505f1e8bca2d9
Gerson Aranibar <aranibargerson28@gmail.com>
2021-11-06 21:12:39 -0500

    update workshop

--- sesion20/ecommerce/core/admin.py
+++ sesion20/ecommerce/core/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from core.models import Product, Category
 
 # Register your models here.
+class ProductAdmin(admin.ModelAdmin):
+    pass
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'subtitle', 'main_picture')
+
+admin.site.register(Product, ProductAdmin)
+admin.site.register(Category, CategoryAdmin)
--- sesion20/ecommerce/core/models.py
+++ sesion20/ecommerce/core/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+class Category(models.Model):
+    title = models.CharField(max_length=100)
+    subtitle = models.CharField(max_length=150)
+    main_picture = models.ImageField(upload_to="categories/")
+
+    def __str__(self):
+        return self.title
+    
+class Product(models.Model):
+    title = models.CharField(max_length=100)
+    price = models.FloatField()
+    main_picture = models.ImageField(upload_to="products/")
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name="products")
